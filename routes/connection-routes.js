11d429a8932138d2c86bcfe5d635327291ee7fa6
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2020-02-13 06:31:40 +0100

    added the ability for users to follow and unfollow each other

--- routes/connection-routes.js
+++ routes/connection-routes.js
@@ -44,4 +44,90 @@ router.post("/follow", (req, res) => {
     });
 });
 
+router.post("/unfollow", (req, res) => {
+    const { whoToUnfollow, byWhom } = req.body;
+    Connections.findOne(
+        {
+            $or: [
+                { follower: whoToUnfollow, followee: byWhom },
+                { follower: byWhom, followee: whoToUnfollow }
+            ]
+        },
+        (err, foundConnection) => {
+            if (err) {
+                return res.json({
+                    error: true,
+                    message: "Something went wrong"
+                });
+            }
+
+            if (!foundConnection) {
+                return res.json({
+                    error: true,
+                    message: "No connections has been made"
+                });
+            }
+            const { _id, follower, followee, mutual_connection } = foundConnection;
+
+            // now that a connection is found
+
+            // if the initiator of the unfollow action initiated the connection in the first place and mutual connection is false
+            if (follower._id === byWhom) {
+                // if the followee actually follows back
+                if (mutual_connection === true) {
+                    // swap the follower and the followee then set mutual relationship to false
+                    foundConnection.follower = followee;
+                    foundConnection.followee = follower;
+                    foundConnection.mutual_connection = false;
+                    foundConnection
+                        .save()
+                        .then(() => {
+                            res.json({
+                                error: false,
+                                message: "You unfollowed them"
+                            });
+                        })
+                        .catch((err) => console.log(err));
+                } else {
+                    // drop the connection column
+                    Connections.findByIdAndDelete(_id, (err, done) => {
+                        if (err) {
+                            return res.json({
+                                error: true,
+                                message: "Oops something went wrong"
+                            });
+                        }
+
+                        if (!done) {
+                            return res.json({
+                                error: true,
+                                message: "Ooops please try again"
+                            });
+                        }
+
+                        res.json({
+                            error: false,
+                            message: "You have unfollowed them"
+                        });
+                    });
+                }
+            } else {
+                // the user did not initiate the connection
+                foundConnection.mutual_connection = false;
+                foundConnection
+                    .save()
+                    .then(() => {
+                        res.json({
+                            error: null,
+                            message: "You have successfully unfollowed them"
+                        });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+        }
+    );
+});
+
 module.exports = router;
